8edc3c79e0f9aae4393fd1413dfd02bcb141c38e
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2016-11-01 22:49:57 -0400

    Add files via upload

---
+++ Test-ADUser.ps1
@@ -0,0 +1,22 @@
+﻿function Test-User {
+    param (
+        [parameter(Mandatory=$True)] [string] $UserName
+    )
+    try {
+        Get-ADUser $UserName -ErrorAction Stop | Out-Null
+    }
+    catch {
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+
+        if ($ErrorMessage -match "Cannot find") {
+            return $false
+            break
+        }
+    }
+    finally {
+        
+    }
+    return $true
+}
+
