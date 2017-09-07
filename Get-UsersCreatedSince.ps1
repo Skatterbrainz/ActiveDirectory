fb30492e755929d71aae87e92e0acac1f3e673af
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2017-09-06 21:24:31 -0400

    Update Get-UsersCreatedSince.ps1

--- Get-UsersCreatedSince.ps1
+++ Get-UsersCreatedSince.ps1
@@ -1,5 +1,15 @@
 #requires -modules ActiveDirectory
 #requires -version 2
+<#
+.SYNOPSIS
+    Returns AD user accounts created on or after a specified date
+.DESCRIPTION
+    Returns AD user accounts created on or after a specified date
+.PARAMETER SinceWhen
+    [datetime] (required) Date to query for accounts
+.EXAMPLE
+    Get-UsersCreatedSince.ps1 -SinceWhen 2/1/2016
+#>
 
 param (
     [parameter(Mandatory=$True, HelpMessage="On or After Date")]
