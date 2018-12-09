c86dbed54e788af8692e845afbf3676033e3760e
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-09 12:02:07 -0500

    Update Get-AdsUsers.ps1

--- Get-AdsUsers.ps1
+++ Get-AdsUsers.ps1
@@ -1,3 +1,4 @@
+function Get-ADsUsers {
 <#
 .DESCRIPTION
     Returns AD LDAP information for User accounts
@@ -13,47 +14,52 @@
     1.0.0 - DS - Initial release
     1.0.1 - DS - Added UserName parameter for focused search
 #>
-
-[CmdletBinding()]
-param (
-    [parameter(Mandatory=$False, HelpMessage="Optional user name")]
-    [string] $UserName = ""
-)
-$pageSize = 1000
-if ([string]::IsNullOrEmpty($UserName)) {
-    $as = [adsisearcher]"(objectCategory=User)"
-}
-else {
-    $as = [adsisearcher]"(&(objectCategory=User)(sAMAccountName=$UserName))"
-}
-$as.PropertiesToLoad.Add('cn') | Out-Null
-$as.PropertiesToLoad.Add('lastlogonTimeStamp') | Out-Null
-$as.PropertiesToLoad.Add('whenCreated') | Out-Null
-$as.PropertiesToLoad.Add('department') | Out-Null
-$as.PropertiesToLoad.Add('title') | Out-Null
-$as.PropertiesToLoad.Add('mail') | Out-Null
-$as.PropertiesToLoad.Add('manager') | Out-Null
-$as.PropertiesToLoad.Add('employeeID') | Out-Null
-$as.PropertiesToLoad.Add('displayName') | Out-Null
-$as.PropertiesToLoad.Add('distinguishedName') | Out-Null
-$as.PageSize = 1000
-$results = $as.FindAll()
-foreach ($item in $results) {
-    $cn = ($item.properties.item('cn') | Out-String).Trim()
-    [datetime]$created = ($item.Properties.item('whenCreated') | Out-String).Trim()
-    $llogon = ([datetime]::FromFiletime(($item.properties.item('lastlogonTimeStamp') | Out-String).Trim())) 
-    $ouPath = ($item.Properties.item('distinguishedName') | Out-String).Trim() -replace $("CN=$cn,", "")
-    $props  = [ordered]@{
-        Name        = $cn
-        DisplayName = ($item.Properties.item('displayName') | Out-String).Trim()
-        Title       = ($item.Properties.item('title') | Out-String).Trim()
-        Department  = ($item.Properties.item('department') | Out-String).Trim()
-        EmployeeID  = ($item.Properties.item('distinguishedName') | Out-String).Trim()
-        Email       = ($item.Properties.item('mail') | Out-String).Trim()
-        Manager     = ($item.Properties.item('manager') | Out-String).Trim()
-        OUPath      = $ouPath
-        Created     = $created
-        LastLogon   = $llogon
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$False, HelpMessage="Optional user name")]
+        [string] $UserName = ""
+    )
+    $pageSize = 1000
+    if ([string]::IsNullOrEmpty($UserName)) {
+        $as = [adsisearcher]"(objectCategory=User)"
     }
-    New-Object psObject -Property $props
+    else {
+        $as = [adsisearcher]"(&(objectCategory=User)(sAMAccountName=$UserName))"
+    }
+    [void]$as.PropertiesToLoad.Add('cn')
+    [void]$as.PropertiesToLoad.Add('sAMAccountName')
+    [void]$as.PropertiesToLoad.Add('lastlogonTimeStamp')
+    [void]$as.PropertiesToLoad.Add('whenCreated')
+    [void]$as.PropertiesToLoad.Add('department')
+    [void]$as.PropertiesToLoad.Add('title')
+    [void]$as.PropertiesToLoad.Add('mail')
+    [void]$as.PropertiesToLoad.Add('manager')
+    [void]$as.PropertiesToLoad.Add('employeeID')
+    [void]$as.PropertiesToLoad.Add('displayName')
+    [void]$as.PropertiesToLoad.Add('distinguishedName')
+    [void]$as.PropertiesToLoad.Add('memberof')
+    $as.PageSize = 1000
+    $results = $as.FindAll()
+    foreach ($item in $results) {
+        $cn = ($item.properties.item('cn') | Out-String).Trim()
+        [datetime]$created = ($item.Properties.item('whenCreated') | Out-String).Trim()
+        $llogon = ([datetime]::FromFiletime(($item.properties.item('lastlogonTimeStamp') | Out-String).Trim())) 
+        $ouPath = ($item.Properties.item('distinguishedName') | Out-String).Trim() -replace $("CN=$cn,", "")
+        $props  = [ordered]@{
+            Name        = $cn
+            UserName    = ($item.Properties.item('sAMAccountName') | Out-String).Trim()
+            DisplayName = ($item.Properties.item('displayName') | Out-String).Trim()
+            Title       = ($item.Properties.item('title') | Out-String).Trim()
+            Department  = ($item.Properties.item('department') | Out-String).Trim()
+            DN          = ($item.Properties.item('distinguishedName') | Out-String).Trim()
+            EmployeeID  = ($item.Properties.item('employeeid') | Out-String).Trim()
+            Email       = ($item.Properties.item('mail') | Out-String).Trim()
+            Manager     = ($item.Properties.item('manager') | Out-String).Trim()
+            Groups      = $item.Properties.item('memberof')
+            OUPath      = $ouPath
+            Created     = $created
+            LastLogon   = $llogon
+        }
+        New-Object psObject -Property $props
+    }
 }
