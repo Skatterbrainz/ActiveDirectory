f02a14cde5ce5407b73aae08864e9349aad2bffb
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2026-05-21 11:44:34 -0400

    fix: correct function call in Get-ADGroupNestedMember for nested group retrieval

--- users/Get-ADGroupNestedMember.ps1
+++ users/Get-ADGroupNestedMember.ps1
@@ -2,6 +2,7 @@ function Get-ADGroupNestedMember {
 	param (
 		[parameter(Mandatory=$True, ValueFromPipeline, HelpMessage="Name of Group")]
 		[ValidateNotNullOrEmpty()]
+		[Alias("Name")]
 		[string] $GroupName
 	)
 	$Members = (Get-ADGroupMember -Identity $GroupName).samAccountName
@@ -10,7 +11,7 @@ function Get-ADGroupNestedMember {
 			Write-Host "user: $Member ($GroupName)"
 			## Test to see if the group member is a group itself
 			if (Get-ADGroup -Identity $Member) {
-				Get-MyADGroupMember -GroupName $Member
+				Get-ADGroupNestedMember -GroupName $Member
 			}
 		}
 		catch {}
